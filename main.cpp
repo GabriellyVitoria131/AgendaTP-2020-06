273f0eba333d14d569bb48fdbcc555eec6d11fc1
Gabrielly de Oliveira <gabyvitoria131@gmail.com>
2020-07-14 23:36:05 -0300

    Configurei o "editar compromisso"
    Usei a função Switch para dar ao usuário a opção de editar apenas um dos
    itens e também a de editar todos de uma vez. Também usei o Switch para
    dar ao usuário a opção de confirmar ou cancelar a edição.

--- main.cpp
+++ main.cpp
@@ -79,7 +79,7 @@ int main(int argc, char **argv) {
 				break;
 			case EDITAR_COMPROMISSO:
 				// TODO implementar
-				std::cout << "Falta implementar." << std::endl;
+				editarCompromisso();
 				break;
 		}
 
